e02610634cca82c543e4a980b86aab53117f4954
Andre-Mateus-Naveca <95716954+Andre-Mateus-Naveca@users.noreply.github.com>
2022-03-04 17:02:10 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+console.log("Meu primeiro projeto no console");
+
+var first_name = "Angry";
+console.log("First Name: " + first_name);
+
+var last_name = "Birds";
+console.log("Last Name: " + last_name);
+
+var a = 110;
+console.log("Valor a: " + a);
+
+var b = 11;
+console.log("Valor b: " + b);
+
+var c = 101;
+console.log("Valor c: " + c);
+
+var pow = Math.pow(5, 5);
+console.log("5^5= " + pow);
+
+var sqrt = Math.sqrt(100);
+console.log("√100= " + sqrt);
+
+var concat = first_name.concat(" ",  last_name);
+console.log("Full name: " + concat);
+
+var add = a + b + c;
+console.log("a + b + c= " + add);
